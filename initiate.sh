a7683b87164761416395bb818ffb36baf3c45277
shikharguptaskyline <shikhar.gupta@skylinesemi.com>
2022-10-12 19:46:11 +0530

    Added docker-compose installation steps

--- initiate.sh
+++ initiate.sh
@@ -10,6 +10,15 @@ sudo systemctl status docker
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
 sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
+sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+sudo apt update
+sudo apt-get install git
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+sudo apt update
 sudo apt install jenkins
 sudo systemctl start jenkins.service
 #sudo systemctl status jenkins
@@ -18,5 +27,5 @@ sudo ufw allow OpenSSH
 sudo ufw enable
 #sudo ufw status
 #sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+#904ab15bf2214717bbc520858053a70b
 mkdir product360
-sudo apt-get install git
